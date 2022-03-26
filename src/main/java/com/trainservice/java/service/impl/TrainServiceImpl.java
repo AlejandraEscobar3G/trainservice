a7d3eb98a529b772592fc3b03c63b3be19b29119
Alejandra Escobar <ale.fesico@gmail.com>
2022-03-25 20:24:29 -0600

    Declare service implementation class

---
+++ src/main/java/com/trainservice/java/service/impl/TrainServiceImpl.java
@@ -0,0 +1,29 @@
+package com.trainservice.java.service.impl;
+
+import com.trainservice.java.dto.ResponseDto;
+import com.trainservice.java.dto.TrainDetailsDto;
+import com.trainservice.java.dto.TrainDetailsResponseDto;
+import com.trainservice.java.dto.TrainResponseDto;
+import com.trainservice.java.service.TrainService;
+
+public class TrainServiceImpl implements TrainService {
+
+	@Override
+	public TrainResponseDto getAllTrains() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseDto saveTrain(TrainDetailsDto trainDetailsDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TrainDetailsResponseDto getTrainById(Integer trainId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
